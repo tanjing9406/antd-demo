7fd35b66537edadc51421b9be680980f4d1d7947
tanjing <1157185306@qq.com>
2020-09-23 22:07:25 +0800

    add antd layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import { Layout } from 'antd';
+import 'antd/dist/antd.css'
+import './App.css'
+
+const { Header, Footer, Sider, Content } = Layout;
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        {/* Using the `component` prop */}
-        <Route path="/home" component={Home} />
+    <Layout style={{height: '100%'}}>
+      <Header style={{color: '#fff'}}>Header</Header>
+      <Layout>
+        <Sider className="site-layout-background fx1">Sider</Sider>
+        <Content>
+          <Router>
+            <Switch>
+              {/* Using the `component` prop */}
+              <Route path="/home" component={Home} />
 
-        {/* Using the `render` prop */}
-        <Route
-          path="/about"
-          render={({ match }) => <About />}
-        />
-      </Switch>
-    </Router>
+              {/* Using the `render` prop */}
+              <Route
+                path="/about"
+                render={({ match }) => <About />}
+              />
+            </Switch>
+          </Router>
+        </Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+    
   );
 }
 
