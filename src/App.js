14e0efffa65023f93e1e409a4ea73f77c9189798
tanjing <1157185306@qq.com>
2020-09-27 09:12:50 +0800

    add menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import 'antd/dist/antd.css'
 import './App.css'
 
 const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
 
 function App() {
   return (
@@ -13,23 +20,26 @@ function App() {
       <Layout style={{height: '100%'}}>
         <Header style={{color: '#fff', fontSize: 24}}>奥利给五金ERP管理系统</Header>
         <Layout>
-          <Sider className="site-layout-background fx1">
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/home">材料管理</Link>
-                </li>
-                <li>
-                  <Link to="/about">生产管理</Link>
-                </li>
-                <li>
-                  <Link to="/home">仓库管理</Link>
-                </li>
-                <li>
-                  <Link to="/about">档案管理</Link>
-                </li>
-              </ul>
-            </nav>
+          <Sider className="site-layout-background fx1" collapsible collapsed={false}>
+            <Menu theme="dark" defaultSelectedKeys={['1']} defaultOpenKeys={['sub2']} mode="inline">
+              <SubMenu key="sub1" icon={<UserOutlined />} title="材料管理">
+                <Menu.Item key="3"><Link to="/home">材料1</Link></Menu.Item>
+                <Menu.Item key="4"><Link to="/about">材料2</Link></Menu.Item>
+                <Menu.Item key="5"><Link to="/home">材料3</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<TeamOutlined />} title="生产管理">
+                <Menu.Item key="1"><Link to="/home">生产计划量</Link></Menu.Item>
+                <Menu.Item key="2"><Link to="/about">压铸情况登记</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" icon={<DesktopOutlined />} title="仓库管理">
+                <Menu.Item key="6"><Link to="/home">冲压情况登记</Link></Menu.Item>
+                <Menu.Item key="7"><Link to="/home">装配工序管理</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub4" icon={<FileOutlined />} title="档案管理">
+                <Menu.Item key="8"><Link to="/home">外发抛光工序管理</Link></Menu.Item>
+                <Menu.Item key="9"><Link to="/home">冲压完工情况</Link></Menu.Item>
+              </SubMenu>
+            </Menu>
           </Sider>
           <Content>
               <Switch>
