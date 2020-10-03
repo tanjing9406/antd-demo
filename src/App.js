d2aa514b1990a1507fe878dea0a83964f5234ecc
tanjing <1157185306@qq.com>
2020-10-03 16:06:30 +0800

    add login page

--- src/App.js
+++ src/App.js
@@ -7,25 +7,31 @@ import 'antd/dist/antd.css'
 import './App.css'
 
 import { AuSider } from './components'
+import LoginPage from './pages/loginpage'
 
 const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <Router>
-      <Layout style={{height: '100%'}}>
-        <Header style={{color: '#fff', fontSize: 24}}>奥利给五金ERP管理系统</Header>
-        <Layout>
-          <AuSider />
-          <Content>
-              <Switch>
-                <Route path="/home" component={Home} />
-                <Route path="/about" render={({ match }) => <About />} />
-              </Switch>
-          </Content>
-        </Layout>
-        <Footer><div style={{display: 'flex', justifyContent: 'space-between'}}><span>登录用户：魏岁奥（普通用户）</span><span>登录时间：2020-09-25 09:10</span><span>记录数：0</span><span>连接状态：在线</span></div></Footer>
-      </Layout>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/">
+          <Layout style={{ height: '100%' }}>
+            <Header style={{ color: '#fff', fontSize: 24 }}>奥利给五金ERP管理系统</Header>
+            <Layout>
+              <AuSider />
+              <Content>
+                <Switch>
+                  <Route path="/home" component={Home} />
+                  <Route path="/about" render={({ match }) => <About />} />
+                </Switch>
+              </Content>
+            </Layout>
+            <Footer><div style={{ display: 'flex', justifyContent: 'space-between' }}><span>登录用户：魏岁奥（普通用户）</span><span>登录时间：2020-09-25 09:10</span><span>记录数：0</span><span>连接状态：在线</span></div></Footer>
+          </Layout>
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/loginpage/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LoginPage() {
+    return (<div>登录页面，敬请期待</div>)
+}
