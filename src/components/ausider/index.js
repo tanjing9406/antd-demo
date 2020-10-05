ec275efed5b522984dcbccd0086c8ca005562f1f
tanjing <1157185306@qq.com>
2020-10-06 02:54:59 +0800

    bugfix: default selected key issue

--- src/components/ausider/index.js
+++ src/components/ausider/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import {
     UserOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
+import { withRouter } from 'react-router'
 
 import MenuConfig from '../../config/menu.config'
 
@@ -12,9 +13,10 @@ import './style.css'
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
-const AuSider = () => {
-    const pathname = window.location.pathname.slice(1).split('/')
+const AuSider = (props) => {
+    const pathname = props.location.pathname.slice(1).split('/')
     const [collapsed, setCollapsed] = useState(false)
+    const [selectedKeys, setSelectedKeys] = useState([pathname.join('-')])
     const [openKeys, setOpenKeys] = useState([pathname[0]])
 
     const onOpenChange = (oKeys) => {
@@ -25,10 +27,13 @@ const AuSider = () => {
         const nextOpenKeys = oKeys.find(key => openKeys.indexOf(key) === -1)
         setOpenKeys([nextOpenKeys])
     }
-
+    useEffect(() => {
+        setSelectedKeys([pathname.join('-')])
+        setOpenKeys([pathname[0]])
+    }, [props.location.pathname])
     return (
         <Sider className="site-layout-background fx1" collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-            <Menu theme="dark" defaultSelectedKeys={[pathname.join('-')]} openKeys={openKeys} onOpenChange={onOpenChange} mode="inline">
+            <Menu theme="dark" selectedKeys={selectedKeys} openKeys={openKeys} onOpenChange={onOpenChange} mode="inline">
                 {MenuConfig.map((menuItem) => (
                     <SubMenu key={menuItem.id} icon={<UserOutlined />} title={menuItem.title}>
                         {menuItem.subMenu.map(sub => <Menu.Item key={sub.id}><Link to={sub.url}>{sub.title}</Link></Menu.Item>)}
@@ -39,4 +44,4 @@ const AuSider = () => {
     )
 }
 
-export default AuSider
+export default withRouter(AuSider)
