3bd2cf3f64e06f46d131986ad1668139c6dd992a
tanjing <1157185306@qq.com>
2020-10-08 23:07:59 +0800

    add a demo page

--- src/config/menu.config.js
+++ src/config/menu.config.js
@@ -1,3 +1,5 @@
+import DragPage from '../pages/demo/drag'
+
 import MenuMaterial from './menuconfigs/menu.config.material'
 import MenuProd from './menuconfigs/menu.config.production'
 import MenuStoreHouse from './menuconfigs/menu.config.storehouse'
@@ -7,5 +9,17 @@ export default [
     ...MenuMaterial,
     ...MenuProd,
     ...MenuStoreHouse,
-    ...MenuFile
+    ...MenuFile,
+    {
+        id: 'demo',
+        title: '演示示例',
+        subMenu: [
+            {
+                id: 'demo-1',
+                title: '拖动',
+                url: '/demo/1',
+                component: DragPage
+            }
+        ] 
+    }
 ]
---
+++ src/pages/demo/drag.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DragPage = () => {
+    return (
+        <div>hhhh</div>
+    )
+}
+
+export default DragPage 
