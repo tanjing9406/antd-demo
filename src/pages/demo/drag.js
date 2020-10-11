0080d9cc32675bd38ad3d97f9e4665591e7ea02c
tanjing <1157185306@qq.com>
2020-10-11 13:41:51 +0800

    drag demo

--- src/pages/demo/drag.js
+++ src/pages/demo/drag.js
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './style.less'
 
 const DragPage = () => {
+    const [cn1, setCn1] = useState('')
+    const [cn2, setCn2] = useState('')
+    const [lineOpacity, setLineOpacity] = useState(0)
+    const [dragging, setDragging] = useState(false)
+    const [pt1, setpt1] = useState([60, 60])
+    const [pt2, setpt2] = useState([360, 60])
+    const [offset, setOffset] = useState([0, 0])
+    const [isClick, setIsClick] = useState(true)
+
+    const toggleClass = (cn, fn) => {
+        if (!isClick) {
+            return
+        }
+        if (!cn) {
+            fn('active')
+            return
+        }
+        fn('')
+    }
+
+    const handleMouseDown = (e) => {
+        const { clientX, clientY } = e
+        setDragging(true)
+        setIsClick(true)
+        setOffset([-clientX, -clientY])
+    }
+    const handleMouseMove = (e, point, setPoint) => {
+        e.persist()
+        if (dragging) {
+            setIsClick(false)
+            const { clientX, clientY } = e
+            const [x, y] = point
+            setPoint([x + clientX + offset[0], y + clientY + offset[1]])
+            setOffset([-clientX, -clientY])
+        }
+    }
+    const handleMouseUp = (e) => {
+        setOffset([0, 0])
+        setDragging(false)
+    }
+
+    useEffect(() => {
+        if (cn1 && cn2) {
+            setLineOpacity(1)
+            return
+        }
+        setLineOpacity(0)
+    }, [cn1, cn2])
+
     return (
-        <div>hhhh</div>
+        <div className="drag-demo">
+            <svg width="100%" height="100%"
+                viewBox="0 0 100% 100%"
+                xmlns="http://www.w3.org/2000/svg">
+                <line x1={pt1[0]} y1={pt1[1]} x2={pt2[0]} y2={pt2[1]} stroke="darkred" opacity={lineOpacity} />
+                <circle cx={pt1[0]} cy={pt1[1]} r="50" className={cn1} fill="pink"
+                    onClick={() => toggleClass(cn1, setCn1)}
+                    onMouseDown={handleMouseDown}
+                    onMouseMove={(e) => handleMouseMove(e, pt1, setpt1)}
+                    onMouseUp={handleMouseUp}
+                />
+                <circle cx={pt2[0]} cy={pt2[1]} r="50" fill="pink" className={cn2}
+                    onClick={() => toggleClass(cn2, setCn2)}
+                    onMouseDown={handleMouseDown}
+                    onMouseMove={(e) => handleMouseMove(e, pt2, setpt2)}
+                    onMouseUp={handleMouseUp}
+                />
+            </svg>
+        </div>
     )
 }
 
